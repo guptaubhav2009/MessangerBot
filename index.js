2b9e93d11eb839f8142b7f2eed22fe427598a667
Anubhav Kumar Gupta <guptanubhav2009@gmail.com>
2016-10-03 18:54:39 +0000

    fixed the extra bracket

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-			console.log("In post webhook " +event.message.text));
+			console.log("In post webhook " + event.message.text);
 			postWatsonRequest(event, event.message.text);
            // sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
         }
